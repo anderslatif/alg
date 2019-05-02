b61f31b49948fb905119c1eed48dfcc533ac934c
anders <al@mobilize-systems.com>
2019-05-02 19:35:49 +0200

    Major refactoring

---
+++ setup.py
@@ -0,0 +1,16 @@
+import setuptools
+
+setuptools.setup(
+    name='alg',
+    version='1.0',
+    author='Anders Latif',
+    author_email='anderslatif@gmail.com',
+    description='The more stupid GIT',
+    packages=setuptools.find_packages(),
+    scripts=['src/libalg.py'],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
